9605d7d766513bfd0665584417b68c9e303cf236
BDanalysis <41245376+BDanalysis@users.noreply.github.com>
2020-01-10 21:41:02 +0800

    Update Density.py

--- software/Microbe-SE/bin/Density.py
+++ software/Microbe-SE/bin/Density.py
@@ -1,42 +1,35 @@
 # coding:utf-8
 import pysam
 
-'''
-功能： 计算物种浓度
-参数： read总量
-返回： 
-'''
 
-
-Dict={}   #存储预测结果，格式: 物种名称:浓度
+Dict={}   
 
 
 def RefFre():
     N = 0
-    # 统计read总量
+
     file = open('Data/example.reads.fq', 'r')
     for line in file:
         N = N + 1
     file.close()
     N = N / 2
 
-    # 记录浓度
+
     file_out = open('Data/Result/RefFre.txt', 'w')
     file_refno = open('Data/Ref_Align/exist_Species.fasta', 'r')
 
-    ref = ''  # 记录物种序列
-
+    ref = ''  
     line = file_refno.readline()
     while line != '':
         if line[0] == '>':
-            i = line.strip().split()[0][1:]  # 物种号
+            i = line.strip().split()[0][1:] 
 
-            name = (line.strip().split()[1]).split(';')[-1]  # 物种
+            name = (line.strip().split()[1]).split(';')[-1] 
 
             line = file_refno.readline()
             ref = line.strip()
 
-            sum = 0  # 物种下read的总和
+            sum = 0 
 
             file_in = pysam.AlignmentFile('Data/Ref_Align/pre_Density.bam', 'rb')
             for r in file_in.fetch(i, 0, len(ref)):
@@ -60,4 +53,4 @@ def RefFre():
 
 
 
-RefFre()  # 计算ref的频率
+RefFre() 
