60026a5c9ef71be027d9c428824cc893308bea3d
BDanalysis <41245376+BDanalysis@users.noreply.github.com>
2020-01-12 10:06:41 +0800

    Update ProgramRun.sh

--- software/Microbe-PE/bin/ProgramRun.sh
+++ software/Microbe-PE/bin/ProgramRun.sh
@@ -1,4 +1,3 @@
-#运行程序
 bwa index Data/Ref_Align/TotalRef1.fasta
 bwa mem Data/Ref_Align/TotalRef1.fasta Data/example.reads1.fq Data/example.reads2.fq > Data/Ref_Align/example.reads.fq.sam
 
@@ -7,7 +6,6 @@ python3 ExtractRef.py
 
 python3 ReadLikelihood.py
 
-#预测物种存在性
 samtools view -Sb Data/Ref_Align/example.reads.sam > Data/Ref_Align/example.reads1.bam
 samtools sort -O bam -o Data/Ref_Align/example.reads.bam Data/Ref_Align/example.reads1.bam
 samtools index Data/Ref_Align/example.reads.bam
@@ -15,17 +13,15 @@ samtools index Data/Ref_Align/example.reads.bam
 python3 RefCharacter.py
 python3 ModelPredict.py
 
-#回收过滤的read
+
 cp Data/Result/exist_Species.fasta Data/Ref_Align/exist_Species.fasta
 cp Data/Result/Recovery.fastq Data/Ref_Align/Recovery.fastq
 
 bwa index Data/Ref_Align/exist_Species.fasta
 bwa mem Data/Ref_Align/exist_Species.fasta Data/Ref_Align/Recovery.fastq > Data/Ref_Align/Recovery.sam
 
-#合并两种比对结果，估计浓度
 python3 Combine_Sam.py
 
-#从sam中建立bam，统计物种浓度
 samtools view -Sb Data/Ref_Align/pre_Density.sam > Data/Ref_Align/pre_Density1.bam
 samtools sort -O bam -o Data/Ref_Align/pre_Density.bam Data/Ref_Align/pre_Density1.bam
 samtools index Data/Ref_Align/pre_Density.bam
